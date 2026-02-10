14922e54b89513bab76419924384067c462050f0
Emmanuel <emmanuel@academia.edu>
2026-02-10 09:43:11 -0800

    use prettier for react files

--- init.lua
+++ init.lua
@@ -700,7 +700,9 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         ruby = { 'rubocop', 'rubyfmt', stop_after_first = true },
       },
     },

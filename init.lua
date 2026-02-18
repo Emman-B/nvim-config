b01ee0d96bf9601d4dd33444dc4a3ab0238c82c4
Emmanuel <emmanuel@academia.edu>
2026-02-17 17:33:34 -0800

    Added nvim-ts-autotag

--- init.lua
+++ init.lua
@@ -181,6 +181,13 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
   'ThePrimeagen/vim-be-good',
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  }, -- handles auto-closing HTML tags and smarter HTML tag renaming (when using ciw, for example)
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
